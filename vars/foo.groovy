0b94c51538e9e4ce73c985c60e6084e571b0bbca
sha13033 <salmasaari@esri.com>
2023-01-06 11:37:51 -0800

    Push branch

---
+++ vars/foo.groovy
@@ -0,0 +1,14 @@
+// House keeping:
+// --------------
+// 1. Anything in vars/ is going to be a global-var 
+// from Jenkins' perspective.
+//
+// 2. The name of this file = the name of the step
+// we want to call in the pipeline file.
+//
+// 3. This file has to be .groovy but it doesn't
+// have to be a Groovy script.
+//
+// 4. You can create a '.txt' that matches the
+// .groovy file name nd that'd be the help
+// that'd shows up in the global variables.
